609756ddc544e9ce9180517bb3cee4219a53f7bd
pmbrian <pmbrian@hotmail.com>
2026-01-07 17:29:02 -0600

    fix cv

--- React/ts-portforlio/src/Profile/Profile.tsx
+++ React/ts-portforlio/src/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import CardExp from "./CardExp"
 
 export default function Profile() {
 
-    const CV_URL: string = "https://mini-enigma.vercel.app/minienigma/cv";
+    const CV_URL: string = "https://mini-enigma.vercel.app/static/CV_BrayhamPavonMartell.pdf";
 
     return (
         <section id="Profile"
@@ -127,7 +127,7 @@ export default function Profile() {
                     <p className="text-xl text-text-dark">¿Interesado en mi perfil?
                         <br /> <small className="text-sm">Descarga aquí mi CV para ver más detalles</small>
                     </p>
-                    <a href={CV_URL} target="_blank" rel="noopener noreferrer" download="BrayhamPavonMartell_CV"
+                    <a href={CV_URL} target="_blank" rel="noopener noreferrer" download="BrayhamPavonMartell_CV.pdf"
                     className="bg-primary-background
                     px-10 py-3 rounded-2xl border-2 border-border-primary-green
                     transition-all duration-300 ease-in-out
